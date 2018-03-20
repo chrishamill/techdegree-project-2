ffbcdd5dd516e2c2a862fde5b56fca229740d8f4
Chris J. Hamill <chris@silviculture.co.uk>
2018-03-20 11:15:34 +0000

    Initial

---
+++ main.js
@@ -0,0 +1,23 @@
+
+
+ const studentPage = function showPage (/* arguments here for page number and student list */) {
+  // first hide all students on the page
+    const toHide = $('.student-list').hide();
+    // Then loop through all students in our student list argument
+      for (i = 0; i < 10; i += 1) {
+        studentPage.show();
+      };
+       // if student should be on this page number
+
+       	// show the student
+
+};
+
+function appendPageLinks () {
+
+
+};
+
+function searchList () {
+
+};
