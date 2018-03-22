bd30b2f42ba1bb9efee8a3b0f98e6a50049d2e46
Chris J. Hamill <chris@silviculture.co.uk>
2018-03-22 13:56:04 +0000

    Showing 10 students

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
-const studentList = document.querySelector('.student-list').getElementsByTagName('li');
+// const studentArray = document.querySelector('.student-list');
+// const studentList = studentArray.getElementsByTagName('li');
+const studentList = document.getElementsByTagName('li')
+
 let noOfStudents = studentList.length;
-const pageNumber = function pageLength () {
-    let perPage = noOfStudents[Math.floor / 10];
-  return perPage;
-};
+
+// pageNumber will need to be linked to the class pagination and active
+const pageNumber = 1;
+
 
 function showPage (studentList, pageNumber/* arguments here for page number and student list */) {
   // first hide all students on the page
   $(studentList).hide();
 
     // Then loop through all students in our student list argument
-      for (i = 0; i < studentList.length; i += 1) {
-        if (studentList[i] < 11) {
-        studentList[i].show();
-      }
+      for (let i = 0; i < 10; i += 1) {
+        console.log(studentList[i]);
+
+        // if (studentList[i] < 11) {
+        $(studentList[i]).show();
+      // }
     }
     };
        // if student should be on this page number
